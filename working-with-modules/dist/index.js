54d9f5b91477f91855d65cb4a9b336aafaaac6be
Hai Dinh Tran <haidinhtran@gmail.com>
2024-06-18 14:40:37 +0700

    working with modules

---
+++ working-with-modules/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const utils_js_1 = require("./utils.js");
+console.log("Hello World!");
+console.log((0, utils_js_1.sample)([12, 3, 44, 3, 223, 12321, 1232, 12]));
+console.log((0, utils_js_1.add)(99, 1));
---
+++ working-with-modules/src/index.ts
@@ -0,0 +1,4 @@
+import {add,sample} from "./utils.js"
+console.log("Hello World!");
+console.log(sample([12,3,44,3,223,12321,1232,12]));
+console.log(add(99,1));
